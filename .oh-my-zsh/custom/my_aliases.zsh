ebffa99df18f253c0c8701bf5083bc8e05700a90
deathowl <csergo.balint@ustream.tv>
2013-02-12 11:26:46 +0100

    workbranch delete zsh alias

--- .oh-my-zsh/custom/my_aliases.zsh
+++ .oh-my-zsh/custom/my_aliases.zsh
@@ -1,3 +1,11 @@
 #To store not ustream specific aliases 
 alias sublime="/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl"
-
+alias antal="ant test-all"
+function wbdelete() {
+ 	for var in "$@"
+	do
+		git push origin :$var
+		git checkout master
+		git branch -D $var
+	done
+}
